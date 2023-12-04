c6eeeaa93cdbdff678cd3f17d6603857d0e6d40a
obs1di4n <80841181+fil1pko@users.noreply.github.com>
2023-12-05 00:04:24 +0100

    added orbs

--- src/components/home.vue
+++ src/components/home.vue
@@ -9,13 +9,28 @@
                 <article>
                     <h2>Tortová Slasť</h2>
                     <div class="line"></div>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi facilis quia soluta laudantium numquam, libero accusamus illo. Eligendi iusto animi nostrum molestiae ea at eos minima quis sit, consequuntur vel?</p>
+                    <p>Na dobré si treba počkať!</p>
                 </article>
             </div>
+            <div class="content-wraper">
+                <div class="orb-wraper">
+                    <img src="../assets/pictures/orbLeftUp.svg" alt="Orb left">
+                    <img src="../assets/pictures/orbRightUp.svg" alt="Orb right">
+                </div>
 
-            <img src="../assets/pictures/cake.png" alt="Torta">
+                <div class="image-wraper">
+                    <img src="../assets/pictures/cake.png" alt="Torta">
+                </div>
+
+                <div class="orb-wraper">
+                    <img src="../assets/pictures/orbLeftDown.svg" alt="Orb left">
+                    <img src="../assets/pictures/orbRightDown.svg" alt="Orb right">
+                </div>
+            </div>
         </div>
 
+
+        <!-- macrons -->
         <div class="choice-wraper second">
             <div class="overlay red">
                 <article>
@@ -24,8 +39,21 @@
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Animi facilis quia soluta laudantium numquam, libero accusamus illo. Eligendi iusto animi nostrum molestiae ea at eos minima quis sit, consequuntur vel?</p>
                 </article>
             </div>
+            <div class="content-wraper">
+                <div class="orb-wraper">
+                    <img src="../assets/pictures/orbLeftUp.svg" alt="Orb left">
+                    <img src="../assets/pictures/orbRightUp.svg" alt="Orb right">
+                </div>
 
-            <img src="../assets/pictures/macron.png" alt="Makrónka">
+                <div class="image-wraper">
+                    <img src="../assets/pictures/macron.png" alt="Makrónka">
+                </div>
+
+                <div class="orb-wraper">
+                    <img src="../assets/pictures/orbLeftDown.svg" alt="Orb left">
+                    <img src="../assets/pictures/orbRightDown.svg" alt="Orb right">
+                </div>
+            </div>
         </div>
     </section>
 </template>
@@ -40,11 +68,35 @@
         display: flex;
         justify-content: space-between;
         height: 39.063vw;
-        margin-top: 2.083vw;
+        max-height: 235.125rem;
+        margin-top: 2.083vw; //5rem
         .choice-wraper{
-            width: 48.438vw;
+            width: 48.438vw; //116.25rem
+            max-width: 116.25rem;
             position: relative;
-            @include flex;
+
+            .content-wraper{
+                height: 100%;
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                padding: 0.521vw; //1.25rem
+                .orb-wraper{
+                    width: 100%;
+                    display: flex;
+                    justify-content: space-between;
+                    img{
+                        opacity: 0.8;
+                        width: 5.729vw;  //13.75rem
+                    }
+                }
+                .image-wraper{
+                    width: 100%;
+                    @include flex;
+                }
+            }
+
+
             .overlay{
                 position: absolute;
                 font-family: $charis;
@@ -53,6 +105,7 @@
                 opacity: 0;
                 @include flex;
                 transition: 0.8s;
+                cursor: pointer;
                 &:hover{
                     opacity: 1;
                 }
@@ -60,18 +113,18 @@
                     @include flex;
                     flex-direction: column;
                     max-width: 60%;
-                    gap: 0.313vw;
+                    gap: 0.313vw; //0.75rem
                     h2{
                         font-weight: bold;
-                        font-size: 1.563vw;
+                        font-size: 1.563vw; //3.75rem
                     }
                     p{
                         text-align: center;
-                        font-size: 0.833vw;
+                        font-size: 0.833vw; //2rem
                     }
                     .line{
-                        height: 0.052vw;
-                        width: 4.219vw;
+                        height: 0.052vw; //0.125rem 
+                        width: 4.219vw; //10.125rem
                         background-color: $black;
                     }
                     .line2{
