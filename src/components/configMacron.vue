031d358911f8774d687033c0693d41d29ad077fb
obs1di4n <80841181+fil1pko@users.noreply.github.com>
2023-12-10 17:03:09 +0100

    macron config nearly done

--- src/components/configMacron.vue
+++ src/components/configMacron.vue
@@ -14,13 +14,68 @@
             <p>macron config</p>
         </div>
 
-        <div class="card">
-            <p>
-                Nakonfiguruj si vlastnú veľkosť balenia, príchuť alebo dokonca aj farbu! <br>
-                Pokiaľ by si mal nejakú špeciálnu požiadavku alebo máš záujem objednať si <br>
-                väčšie množstvo napíš nám správu nie je nič s čím si hravo neporadíme.  
-            </p>
+        <div class="choice-img" style="background-color: red;">
+            <img src="../assets/pictures/macron.png" alt="Tvoja volba">
         </div>
+
+        <article>
+            <!-- intro -->
+            <p>
+                Nakonfiguruj si vlastnú veľkosť balenia, príchuť alebo dokonca aj farbu! Pokiaľ by si mal nejakú špeciálnu požiadavku alebo máš záujem objednať si<br>väčšie množstvo napíš nám správu nie je nič s čím si hravo neporadíme.  
+            </p>
+
+            <!-- volba velkosti -->
+            <p>
+                Vyber si veľkosť.
+            </p>
+            <div class="button-wraper">
+                <div class="button small"> L </div>
+                <div class="button small"> M </div>
+                <div class="button small"> S </div>
+                <div class="button small teaser"> Teaser </div>
+                <p>Počet kusov v balení: <b>30</b></p>
+            </div>
+
+            <!-- volba prichute -->
+            <p>
+                Vyber si nejakú z našich vychytených príchutí.
+            </p>
+            <div class="button-wraper">
+                <div class="button big">
+                    <h6>ČERVENÁ</h6>
+                    <small>Lesná zmes</small>
+                </div>
+                <div class="button big">
+                    <h6>HNEDÁ</h6>
+                    <small>Káva</small>
+                </div>
+                <div class="button big">
+                    <h6>BIELA</h6>
+                    <small>Karamel</small>
+                </div>
+                <div class="button big">
+                    <h6>ZELENÁ</h6>
+                    <small>Pistácie</small>
+                </div>
+            </div>
+
+
+            <!-- email set -->
+            <p>
+                Zadaj mail, v ktorom sa dohodneme na detailoch a dodaní, zároveň žiadame o vyriešenie jednoduchého príkladu aby sme si overrili, že nie si robot. 
+            </p>
+            <div class="button-wraper form-wraper">
+                <form>
+                    <input type="mail" class="longer">
+                    <input type="text" class="shorter">
+                    <button type="submit">Objednať</button>
+                </form>
+            </div>
+
+            <p>
+                Chceš to nakombinovať úple podľa seba? Tu nám napíš svoju predstavu.    
+            </p>
+        </article>
     </section>
 
 </template>
@@ -30,12 +85,66 @@
     @import '../assets/style/main.scss';
 
     #macronConfigurator{
-        .card{
+        width: 100%;
+        justify-content: center;
+        margin: 0 auto;    
+        display: flex;
+        height: 39.063vw; //235.125rem
+        margin-top: 2.083vw; //5rem
+        article{
             background-color: lightgreen;
             font-family: $charis;
             p{
                 font-size: 0.833vw;
             }
+            .button-wraper{
+                display: flex;
+                align-items: center;
+                .button{
+                    @include flex;
+                    background-image: url('../assets/pictures/orbBorder.svg');
+                    background-repeat: no-repeat;
+                    background-size: contain;
+                    cursor: pointer;
+                }
+                h6{
+                    font-size: 0.833vw;
+                }
+                .small{
+                    width: 70px;
+                    height: 49px;
+                }
+
+                .big{
+                    width: 130px;
+                    height: 91px;
+                    flex-direction: column;
+                }
+            }
+            .form-wraper{
+                flex-direction: column;
+                form{
+                    .longer{
+                        all: unset;
+                        width: 280px;
+                        height: 44px;
+                        background-image: url('../assets/pictures/orbBorderLong.svg');
+                        background-repeat: no-repeat;
+                        background-size: cover;
+                    }
+                    .shorter{
+                        all: unset;
+                        height: 44px;
+                        background-image: url('../assets/pictures/orbBorderLong2.svg');
+                        background-repeat: no-repeat;
+                        background-size: cover;
+                    }
+                }
+            }
+        }
+        .choice-img{
+            display: flex;
+            align-items: center;
         }
     }
 </style>
